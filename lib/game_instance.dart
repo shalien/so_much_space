5187fe52e8e90d4981b4afa826679573bb846633
Ophélien DUPARC <ophelien.duparc@projetretro.io>
2022-07-12 12:59:19 +0200

    Change to real collisions

--- lib/game_instance.dart
+++ lib/game_instance.dart
@@ -1,14 +1,16 @@
 
 
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 
 import 'package:so_much_space/player_triangle.dart';
 
-class GameInstance extends FlameGame {
+class GameInstance extends FlameGame with HasCollisionDetection {
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
+    await add(ScreenHitbox());
 
     await add(PlayerTriangle());
   }
--- lib/player_triangle.dart
+++ lib/player_triangle.dart
@@ -1,11 +1,11 @@
 
 import 'dart:math';
 
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
-import 'package:flame/effects.dart';
 import 'package:so_much_space/game_instance.dart';
 
-class PlayerTriangle extends SpriteComponent with HasGameRef<GameInstance> {
+class PlayerTriangle extends SpriteComponent with HasGameRef<GameInstance>, CollisionCallbacks {
 
   PlayerTriangle() : super(size: Vector2.all(32));
 
@@ -14,9 +14,42 @@ class PlayerTriangle extends SpriteComponent with HasGameRef<GameInstance> {
 
   @override
   Future<void> onLoad() async {
-    anchor = Anchor.center;
-    position = Vector2(gameRef.size.x / 2, gameRef.size.y / 2);
+    await add(RectangleHitbox());
+    position = Vector2(Random().nextInt(gameRef.size.x.toInt()).toDouble(), Random().nextInt(gameRef.size.y.toInt()).toDouble());
     sprite = await Sprite.load('shalien.png');
+
+  }
+
+  @override
+  void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
+    // TODO: implement onCollision
+    super.onCollision(intersectionPoints, other);
+
+    if(other is ScreenHitbox) {
+      if(position.y >= gameRef.size.y) {
+        newY = -1;
+      } else if (position.y <= 0) {
+        newY = 1;
+      }
+
+      if(position.x >= gameRef.size.x) {
+        newX =  -1;
+      } else if (position.x <= 0) {
+        newX = 1;
+      }
+
+      print('Boink');
+    }
+  }
+
+  @override
+  void onCollisionEnd(PositionComponent other) {
+    // TODO: implement onCollisionEnd
+    super.onCollisionEnd(other);
+
+    if(other is ScreenHitbox) {
+      print('Not boint');
+    }
   }
 
   @override
@@ -24,23 +57,13 @@ class PlayerTriangle extends SpriteComponent with HasGameRef<GameInstance> {
     // TODO: implement update
     super.update(dt);
 
-    if(position.y >= gameRef.size.y) {
-      newY = -1;
-    } else if (position.y <= 0) {
-      newY = 1;
-    }
+    angle = angle + 0.01;
 
-    if(position.x >= gameRef.size.x) {
-      newX =  -1;
-    } else if (position.x <= 0) {
-      newX = 1;
-    }
-
-    angle = angle + ((newX + newY) / 100);
-
-    scale = Vector2(scale.x + (newX / 100), scale.y + (newY / 100));
+//    scale = Vector2.all(Random().nextInt(2).toDouble());
 
     position = Vector2(position.x + newX, position.y + newY);
+
+
   }
 
   @override
