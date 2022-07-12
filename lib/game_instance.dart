8985f1391e17f657e70723030e5d1b3669acdc60
Ophélien DUPARC <ophelien.duparc@projetretro.io>
2022-07-12 17:05:31 +0200

    Added sprite

--- lib/game_instance.dart
+++ lib/game_instance.dart
@@ -2,17 +2,32 @@
 
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame/parallax.dart';
+import 'package:flutter/widgets.dart';
 
 import 'package:so_much_space/player_triangle.dart';
 
 class GameInstance extends FlameGame with HasCollisionDetection {
 
+  final sprites = [
+    'shalien.png',
+    'shalien2.png',
+    'shalien3.png',
+    'moa.png',
+    'rondo.png',
+    'linoyd.png',
+    'linoyd2.png',
+  ];
+
   @override
   Future<void> onLoad() async {
+    print('${camera.canvasSize.x} ${camera.canvasSize.y} ${camera.viewport.effectiveSize.x} ${camera.viewport.effectiveSize.y}');
     await super.onLoad();
     await add(ScreenHitbox());
 
-    await add(PlayerTriangle());
+    for(var sprite in sprites) {
+      await add(PlayerTriangle(sprite));
+    }
   }
 
 }
--- lib/player_triangle.dart
+++ lib/player_triangle.dart
@@ -1,40 +1,44 @@
 
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:so_much_space/game_instance.dart';
+import 'package:so_much_space/utils.dart';
 
 class PlayerTriangle extends SpriteComponent with HasGameRef<GameInstance>, CollisionCallbacks {
 
-  PlayerTriangle() : super(size: Vector2.all(32));
+  final String spriteName;
+
+  PlayerTriangle(this.spriteName) : super(size: Vector2.all(32));
 
   double newX = 1;
   double newY = 1;
 
   @override
   Future<void> onLoad() async {
-    await add(RectangleHitbox());
+    await add(RectangleHitbox(size: Vector2.all(32)));
     position = Vector2(Random().nextInt(gameRef.size.x.toInt()).toDouble(), Random().nextInt(gameRef.size.y.toInt()).toDouble());
-    sprite = await Sprite.load('shalien.png');
+    sprite = await Sprite.load(spriteName);
+    angle = minMaxDouble(-5.0, 5.0);
 
   }
 
   @override
   void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
-    // TODO: implement onCollision
     super.onCollision(intersectionPoints, other);
 
     if(other is ScreenHitbox) {
-      if(position.y >= gameRef.size.y) {
+      if(position.y + 32 >= gameRef.size.y) {
         newY = -1;
-      } else if (position.y <= 0) {
+      } else if (position.y - 32 <= 0) {
         newY = 1;
       }
 
-      if(position.x >= gameRef.size.x) {
-        newX =  -1;
-      } else if (position.x <= 0) {
+      if(position.x + 32 >= gameRef.size.x) {
+        newX = -1;
+      } else if (position.x - 32 <= 0) {
         newX = 1;
       }
 
@@ -44,7 +48,6 @@ class PlayerTriangle extends SpriteComponent with HasGameRef<GameInstance>, Coll
 
   @override
   void onCollisionEnd(PositionComponent other) {
-    // TODO: implement onCollisionEnd
     super.onCollisionEnd(other);
 
     if(other is ScreenHitbox) {
@@ -54,16 +57,18 @@ class PlayerTriangle extends SpriteComponent with HasGameRef<GameInstance>, Coll
 
   @override
   void update(double dt) {
-    // TODO: implement update
     super.update(dt);
-
-    angle = angle + 0.01;
-
 //    scale = Vector2.all(Random().nextInt(2).toDouble());
 
     position = Vector2(position.x + newX, position.y + newY);
 
+    if(angle <= 0) {
+      angle = angle -dt;
+    } else {
+      angle = angle + dt;
+    }
 
+    paint = Paint()..color = randomColor;
   }
 
   @override
